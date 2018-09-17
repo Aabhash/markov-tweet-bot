b9dfc15d9a3d8843bd33b0269e4141b39a1ee229
Aabhash Dhakal <aabhashd@outlook.com>
2018-09-17 22:59:02 +0545

    [ADD]: Complete ngram implementation

--- src/markov_model.py
+++ src/markov_model.py
@@ -8,7 +8,7 @@ class MarkovModel:
     
     # def clear_data():
 
-    def create_dict(self, content, ngram=2):
+    def create_dict(self, content, ngram=3):
         content = content.split()
         total_words = len(content) - ngram
         for word in range(total_words):
@@ -20,39 +20,28 @@ class MarkovModel:
             
     
     # Return generated text
-    def __get_result(self, n, word1, word2):
+    def __get_result(self, n, words):
         resultant_text = []
         for i in range(n):
-            resultant_text.append(word1)
-            if (word1,word2) in self.corpus:
-                word1, word2 = word2, random.choice(self.corpus[(word1, word2)])
-        resultant_text.append(word2)
+            temp = tuple(words)
+            resultant_text.append(words[0])
+            if temp in self.corpus:
+                for i in range(len(words) - 1):
+                    words[i] = words[i+1]
+                words[len(words)-1] = random.choice(self.corpus[temp])
+        resultant_text.append(words[len(words)-1])
         return resultant_text
 
-    def generate_text(self, keyword = "xyz good"):
+    def generate_text(self):
         if not self.corpus:
             print('Corpus Empty')
-
-        keyword = [keyword]
+            
         keys = self.corpus 
         key_list = list(keys)
         random.shuffle(key_list)
-
-        # Random seed
         seed = random.randint(0, len(list(keys)))
-        word1, word2 = key_list[seed]
-        # if seed != None: 
-        #     while keyword:
-        #         for i in range(len(keys)):
-        #             if keyword[0] in key_list[i] or keyword[0].split() == key_list[i]:
-        #                 word1, word2 = key_list[i]
-        #                 keyword = []
-        #                 break
-        #         if keyword:
-        #             keyword.pop(0)
-
-        resultant_text = self.__get_result(140, word1, word2)
-        # resultant_text = TextProcessor.clean_grammar(resultant_text)
+        words = list(key_list[seed])
+        resultant_text = self.__get_result(140, words)
         return resultant_text
 
 model = MarkovModel()
@@ -62,4 +51,4 @@ with open('../shakespeare.txt', 'r') as reader:
 model.create_dict(content)
 text = model.generate_text()
 # print(model.corpus)
-# print(' '.join(text))
+print(' '.join(text))
